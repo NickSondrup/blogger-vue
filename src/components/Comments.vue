27957a385343578cdce9d376c2c6f552a8e50a89
Quinnton <quinnclimbs@gmail.com>
2021-09-23 16:56:47 -0600

    c

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -8,6 +8,12 @@
 import { computed } from '@vue/runtime-core'
 import { AppState } from '../AppState'
 export default {
+  props: {
+    comment: {
+      type: Object,
+      required: false
+    }
+  },
   setup() {
     return {
       comments: computed(() => AppState.currentComments)
