ba752a637f190e3a612114c83c49bdcd306b18a5
Quinnton <quinnclimbs@gmail.com>
2021-09-23 15:16:51 -0600

    commit

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="card col-5 my-2 selectable">
-    <div class="on-hover position-absolute" style="right: 1rem; top: 1rem" v-if="account.id == blog.creatorId">
-      <i class="mdi mdi-close text-danger f-20 selectable" @click="deleteBlog()"></i>
-    </div>
-    <div class="card-header d-flex flex-column text-center">
-      <div class="row">
-        {{ blog.title }}
+  <router-link :to="{name: 'Blog', params: {blogId: blog.id}}" :title="blog.title">
+    <div class="card col-5 my-2 selectable">
+      <div class="on-hover position-absolute" style="right: 1rem; top: 1rem" v-if="account.id == blog.creatorId">
+        <i class="mdi mdi-close text-danger f-20 selectable" @click="deleteBlog()"></i>
       </div>
-      <div class="row">
-        By: {{ blog.creator.name }}
+      <div class="card-header d-flex flex-column text-center">
+        <div class="row">
+          {{ blog.title }}
+        </div>
+        <div class="row">
+          By: {{ blog.creator.name }}
+        </div>
+      </div>
+      <img :src="blog.imgUrl" alt="">
+      <div class="card-body text-center scrollable-y">
+        {{ blog.body }}
       </div>
     </div>
-    <img :src="blog.imgUrl" alt="">
-    <div class="card-body text-center scrollable-y">
-      {{ blog.body }}
-    </div>
-  </div>
+  </router-link>
 </template>
 
 <script>
