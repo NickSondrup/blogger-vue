ae7f2e202e7097fd711e3f6d2b94f64a4e1d3d41
Quinnton <quinnclimbs@gmail.com>
2021-09-23 14:19:57 -0600

    commmit

---
+++ src/components/Blog.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+    </div>
+    <div class="card-body">
+    </div>
+    <div class="card-footer">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
