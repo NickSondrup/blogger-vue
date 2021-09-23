bcfb7abc50308bd0855705bbd72eaeeaf51cff85
Nick Sondrup <nicholas.sondrup@gmail.com>
2021-09-23 14:35:41 -0600

    blog model complete

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -13,21 +13,5 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home{
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-  .home-card{
-    width: 50vw;
-    > img{
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
-}
+
 </style>
