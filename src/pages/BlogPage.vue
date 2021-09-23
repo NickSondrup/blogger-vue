3e14b9b3f7f3535c886045102b6e2aaae752c01b
Nick Sondrup <nicholas.sondrup@gmail.com>
2021-09-23 15:33:39 -0600

    c

---
+++ src/pages/BlogPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card ">
+    <div class="card-header">
+      this is blog page
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
